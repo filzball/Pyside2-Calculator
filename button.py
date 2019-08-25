d64e29ee0441510161167880cc6dc6837766cf38
Patrick Wieker <filzball91@googlemail.com>
2019-08-25 19:34:08 +0200

    added a Button Class that derives from QToolButton

---
+++ button.py
@@ -0,0 +1,25 @@
+import sys
+from PySide2.QtWidgets import QToolButton, QSizePolicy, QApplication
+from PySide2.QtCore import QSize
+
+
+class Button(QToolButton):
+    def __init__(self, text):
+        super(Button, self).__init__()
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        self.setText(text)
+
+    def sizeHint(self):
+        size = QToolButton.sizeHint(self)
+        width = size.height() + 20
+        height = max(size.width(), size.height())
+        hint = QSize(width, height)
+        print(hint)
+        return hint
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    b = Button('test')
+    b.sizeHint()
+    b.show()
